c9fec37f2eb0424d7803d9cb5db777bae3d4674d
devR <73724930+reytechcode@users.noreply.github.com>
2025-10-26 22:30:12 -0500

    Cambios V1

--- CreateAppointmentActivity.kt
+++ CreateAppointmentActivity.kt
@@ -44,7 +44,6 @@ class CreateAppointmentActivity : AppCompatActivity() {
     private val preferences by lazy {
         PreferenceHelper.defaultPrefs(this)
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_appointment)
@@ -52,16 +51,18 @@ class CreateAppointmentActivity : AppCompatActivity() {
         val btnNext = findViewById<Button>(R.id.btn_siguiente)
         val btnNext2 = findViewById<Button>(R.id.btn_siguiente_dos)
         val btnConfirm = findViewById<Button>(R.id.btn_confirmar)
+
         val cvNext = findViewById<CardView>(R.id.cv_siguiente)
         val cvConfirm = findViewById<CardView>(R.id.cv_confirmar)
         val cvResumen = findViewById<CardView>(R.id.cv_resumen)
+
         val etDescription = findViewById<EditText>(R.id.et_description)
         val etScheduledDate = findViewById<EditText>(R.id.et_fecha)
         val linearLayoutCreateAppointment = findViewById<LinearLayout>(R.id.linearLayout_create_appointment)
 
         btnNext.setOnClickListener{
             if (etDescription.text.toString().length < 3){
-                etDescription.error = "La descripcion es demasido corta"
+                etDescription.error = "La descripción es demasiada corta"
             }else {
                 cvNext.visibility = View.GONE
                 cvConfirm.visibility = View.VISIBLE
@@ -80,14 +81,14 @@ class CreateAppointmentActivity : AppCompatActivity() {
                 cvResumen.visibility = View.VISIBLE
             }
         }
-
         btnConfirm.setOnClickListener{
             performStoreAppointment()
         }
 
         loadSpecialties()
         listenSpecialtyChanges()
-        listenDoctorAndDataChanges()
+        listenDoctorAndDateChanges()
+
     }
 
     private fun performStoreAppointment() {
@@ -97,28 +98,29 @@ class CreateAppointmentActivity : AppCompatActivity() {
         val jwt = preferences["jwt", ""]
         val authorization = "Bearer $jwt"
 
-        val tvConfirmdescription = findViewById<TextView>(R.id.tv_resumen_sintomas)
+        val tvConfirmDescription = findViewById<TextView>(R.id.tv_resumen_sintomas)
         val tvConfirmDate = findViewById<TextView>(R.id.tv_resumen_fecha)
         val tvConfirmTime = findViewById<TextView>(R.id.tv_resumen_hora)
         val tvConfirmType = findViewById<TextView>(R.id.tv_resumen_tipoConsulta)
         val spinnerDoctor = findViewById<Spinner>(R.id.spinner_medico)
-        val spinnerSpecialities = findViewById<Spinner>(R.id.spinner_especialidades)
+        val spinnerSpecialties = findViewById<Spinner>(R.id.spinner_especialidades)
 
-        val description = tvConfirmdescription.text.toString()
+        val description = tvConfirmDescription.text.toString()
         val scheduledDate = tvConfirmDate.text.toString()
         val scheduledTime = tvConfirmTime.text.toString()
         val type = tvConfirmType.text.toString()
         val doctor = spinnerDoctor.selectedItem as Doctor
-        val specialty = spinnerSpecialities.selectedItem as Specialty
+        val specialty = spinnerSpecialties.selectedItem as Specialty
 
-        val call = apiService.storeAppointments(authorization, description, scheduledDate, scheduledTime, type, doctor.id, specialty.id)
-
+        val call = apiService.storeAppointments(authorization, description,
+            scheduledDate, scheduledTime, type,
+            doctor.id, specialty.id)
         call.enqueue(object: Callback<SimpleResponse>{
             override fun onResponse(
                 call: Call<SimpleResponse>,
                 response: Response<SimpleResponse>
             ) {
-                if (response.isSuccessful) {
+                if(response.isSuccessful) {
                     Toast.makeText(this@CreateAppointmentActivity, "La cita se realizó correctamente.", Toast.LENGTH_SHORT).show()
                     finish()
                 }else {
@@ -133,23 +135,23 @@ class CreateAppointmentActivity : AppCompatActivity() {
             }
 
         })
+
     }
 
-    private fun listenDoctorAndDataChanges() {
+    private fun listenDoctorAndDateChanges() {
         val etScheduledDate = findViewById<EditText>(R.id.et_fecha)
-        val spinnerDoctor= findViewById<Spinner>(R.id.spinner_medico)
+        val spinnerDoctor = findViewById<Spinner>(R.id.spinner_medico)
         spinnerDoctor.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(adapter: AdapterView<*>?, pview: View?, position: Int, id: Long) {
+            override fun onItemSelected(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 val doctor = adapter?.getItemAtPosition(position) as Doctor
                 loadHours(doctor.id, etScheduledDate.text.toString())
             }
-
             override fun onNothingSelected(p0: AdapterView<*>?) {
 
             }
         }
-        // FECHAS
-        etScheduledDate.addTextChangedListener(object: TextWatcher{
+        //Fechas
+        etScheduledDate.addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
@@ -167,46 +169,47 @@ class CreateAppointmentActivity : AppCompatActivity() {
     }
 
     private fun loadHours(doctorId: Int, date: String){
+        val tvSelectDoctorAndDate = findViewById<TextView>(R.id.tv_seleccionar_medico_fecha)
 
-            val tvSelectDoctorAndDate = findViewById<TextView>(R.id.tv_seleccionar_medico_fecha)
+        if (date.isEmpty()) {
+            return
+        }
+        val call = apiService.getHours(doctorId, date)
+        call.enqueue(object: Callback<Schedule> {
+            override fun onResponse(call: Call<Schedule>, response: Response<Schedule>) {
+                if (response.isSuccessful) {
+                    val schedule = response.body()
+                    schedule?.let {
+                        tvSelectDoctorAndDate.visibility = View.GONE
 
-
-            if (date.isEmpty()){
-                return
-            }
-            val call = apiService.getHours(doctorId, date)
-            call.enqueue(object: Callback<Schedule> {
-                override fun onResponse(call: Call<Schedule>, response: Response<Schedule>) {
-                    if (response.isSuccessful){
-                        val schedule = response.body()
-                        schedule?.let{
-                            val intervals = it.morning + it.afternoon
-                            val hours = ArrayList<String>()
-                            intervals.forEach{ interval ->
-                                hours.add(interval.start)
-                            }
-                            displayRadioButtons(hours)
+                        val intervals = it.morning + it.afternoon
+                        val hours = ArrayList<String>()
+                        intervals.forEach { interval ->
+                            hours.add(interval.start)
                         }
+                        displayRadioButtons(hours)
                     }
-                }
 
-                override fun onFailure(call: Call<Schedule>, t: Throwable) {
-                    Toast.makeText(applicationContext, "Error: no se pudo cargar las horas", Toast.LENGTH_SHORT).show()
                 }
+            }
 
-            })
+            override fun onFailure(call: Call<Schedule>, t: Throwable) {
+                Toast.makeText(applicationContext, "Error: NO se pudo cargar las horas.", Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
     private fun loadSpecialties() {
         val spinnerSpecialties = findViewById<Spinner>(R.id.spinner_especialidades)
-        val call = apiService.getSpecialities()
+        val call = apiService.getSpecialties()
         call.enqueue(object: Callback<ArrayList<Specialty>>{
             override fun onResponse(
                 call: Call<ArrayList<Specialty>>,
                 response: Response<ArrayList<Specialty>>
             ) {
                 if (response.isSuccessful){
-                    response.body()?.let{
+                    response.body()?.let {
                         val specialties = it.toMutableList()
                         spinnerSpecialties.adapter = ArrayAdapter(this@CreateAppointmentActivity, android.R.layout.simple_list_item_1, specialties)
                     }
@@ -221,46 +224,42 @@ class CreateAppointmentActivity : AppCompatActivity() {
 
     }
 
+    private fun listenSpecialtyChanges() {
+        val spinnerSpecialty = findViewById<Spinner>(R.id.spinner_especialidades)
+        spinnerSpecialty.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapter: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val specialty = adapter?.getItemAtPosition(position) as Specialty
+                LoadDoctors(specialty.id)
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
     private fun LoadDoctors(specialtyId: Int) {
         val spinnerDoctor = findViewById<Spinner>(R.id.spinner_medico)
-        val call = apiService.getDoctor(specialtyId)
+        val call = apiService.getDoctors(specialtyId)
         call.enqueue(object: Callback<ArrayList<Doctor>>{
             override fun onResponse(
                 call: Call<ArrayList<Doctor>>,
                 response: Response<ArrayList<Doctor>>
             ) {
                 if (response.isSuccessful){
-                    response.body()?.let{
+                    response.body()?.let {
                         val doctors = it.toMutableList()
                         spinnerDoctor.adapter = ArrayAdapter(this@CreateAppointmentActivity, android.R.layout.simple_list_item_1, doctors)
                     }
                 }
             }
-
             override fun onFailure(call: Call<ArrayList<Doctor>>, t: Throwable) {
-                Toast.makeText(applicationContext, "Error, no se pudieron cargar los médicos", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "Error, no se pudo cargar los médicos", Toast.LENGTH_SHORT).show()
             }
-
         })
     }
 
-    private fun listenSpecialtyChanges() {
-        val spinnerSpecialty = findViewById<Spinner>(R.id.spinner_especialidades)
-        spinnerSpecialty.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(adapter: AdapterView<*>?, pview: View?, position: Int, id: Long) {
-                val specialty = adapter?.getItemAtPosition(position) as Specialty
-                LoadDoctors(specialty.id)
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-
-        }
-    }
-
     private fun showAppointmentDataToConfirm() {
-        val tvConfirmdescription = findViewById<TextView>(R.id.tv_resumen_sintomas)
+        val tvConfirmDescription = findViewById<TextView>(R.id.tv_resumen_sintomas)
         val tvConfirmSpecialty = findViewById<TextView>(R.id.tv_resumen_especialidad)
         val tvConfirmType = findViewById<TextView>(R.id.tv_resumen_tipoConsulta)
         val tvConfirmDoctorName = findViewById<TextView>(R.id.tv_resumen_medico)
@@ -268,13 +267,13 @@ class CreateAppointmentActivity : AppCompatActivity() {
         val tvConfirmTime = findViewById<TextView>(R.id.tv_resumen_hora)
 
         val etDescription = findViewById<EditText>(R.id.et_description)
-        val spinnerSpecialities = findViewById<Spinner>(R.id.spinner_especialidades)
+        val spinnerSpecialties = findViewById<Spinner>(R.id.spinner_especialidades)
         val radioGroupType = findViewById<RadioGroup>(R.id.radio_group_type)
         val spinnerDoctor = findViewById<Spinner>(R.id.spinner_medico)
         val etScheduledDate = findViewById<EditText>(R.id.et_fecha)
 
-        tvConfirmdescription.text = etDescription.text.toString()
-        tvConfirmSpecialty.text = spinnerSpecialities.selectedItem.toString()
+        tvConfirmDescription.text = etDescription.text.toString()
+        tvConfirmSpecialty.text = spinnerSpecialties.selectedItem.toString()
 
         val selectRadioBtnId = radioGroupType.checkedRadioButtonId
         val selectedRadioType = radioGroupType.findViewById<RadioButton>(selectRadioBtnId)
@@ -283,36 +282,40 @@ class CreateAppointmentActivity : AppCompatActivity() {
         tvConfirmDoctorName.text = spinnerDoctor.selectedItem.toString()
         tvConfirmDate.text = etScheduledDate.text.toString()
         tvConfirmTime.text = selectedRadioButton?.text.toString()
+
     }
 
-    fun onClickScheduleDate(v: View?) {
+    fun onClickScheduledDate(v: View?) {
         val etScheduledDate = findViewById<EditText>(R.id.et_fecha)
-
 
         val year = selectedCalendar.get(Calendar.YEAR)
         val month = selectedCalendar.get(Calendar.MONTH)
         val dayOfMonth = selectedCalendar.get(Calendar.DAY_OF_MONTH)
         val listener = DatePickerDialog.OnDateSetListener{datePicker, y, m, d ->
-            selectedCalendar.set(y,m,d)
-            etScheduledDate.setText(resources.getString(R.string.date_format, y, (m+1).twoDigits(), d.twoDigits()))
+            selectedCalendar.set(y, m, d)
+            etScheduledDate.setText(resources.getString(R.string.date_format,
+                y,
+                (m+1).twoDigits(),
+                d.twoDigits()
+            ))
             etScheduledDate.error = null
 
         }
-
-       val datePickerDialog = DatePickerDialog(this, listener, year, month, dayOfMonth)
-       val datePicker = datePickerDialog.datePicker
-       val calendar = Calendar.getInstance()
-       calendar.add(Calendar.DAY_OF_MONTH, 1)
+        val datePickerDialog = DatePickerDialog(this, listener, year, month, dayOfMonth)
+        val datePicker = datePickerDialog.datePicker
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.DAY_OF_MONTH, 1)
         datePicker.minDate = calendar.timeInMillis
         calendar.add(Calendar.DAY_OF_MONTH, 29)
         datePicker.maxDate = calendar.timeInMillis
-       datePickerDialog.show()
+
+        datePickerDialog.show()
     }
 
     private fun Int.twoDigits()
-    = if(this > 10) this.toString() else "0$this"
+            = if(this >=10) this.toString() else "0$this"
 
-    private fun displayRadioButtons(hours: ArrayList<String>){
+    private fun displayRadioButtons(hours: ArrayList<String>) {
         val radioGroupLeft = findViewById<LinearLayout>(R.id.radio_group_izq)
         val radioGroupRight = findViewById<LinearLayout>(R.id.radio_group_der)
 
@@ -323,7 +326,7 @@ class CreateAppointmentActivity : AppCompatActivity() {
 
         selectedRadioButton = null
 
-        if (hours.isEmpty()){
+        if (hours.isEmpty()) {
             tvNotAvailableHours.visibility = View.VISIBLE
             return
         }
@@ -337,7 +340,7 @@ class CreateAppointmentActivity : AppCompatActivity() {
             radioButton.id = View.generateViewId()
             radioButton.text = it
 
-            radioButton.setOnClickListener { view ->
+            radioButton.setOnClickListener{ view ->
                 selectedRadioButton?.isChecked = false
                 selectedRadioButton = view as RadioButton?
                 selectedRadioButton?.isChecked = true
@@ -349,16 +352,15 @@ class CreateAppointmentActivity : AppCompatActivity() {
                 radioGroupRight.addView(radioButton)
             goToLeft = !goToLeft
         }
-    }
 
+    }
     @SuppressLint("MissingSuperCall")
     override fun onBackPressed() {
-
         val cvNext = findViewById<CardView>(R.id.cv_siguiente)
         val cvConfirm = findViewById<CardView>(R.id.cv_confirmar)
         val cvResumen = findViewById<CardView>(R.id.cv_resumen)
 
-        if (cvResumen.visibility == View.VISIBLE) {
+        if(cvResumen.visibility == View.VISIBLE){
             cvResumen.visibility = View.GONE
             cvConfirm.visibility = View.VISIBLE
         }else if (cvConfirm.visibility == View.VISIBLE){
@@ -366,16 +368,14 @@ class CreateAppointmentActivity : AppCompatActivity() {
             cvNext.visibility = View.VISIBLE
         }else if (cvNext.visibility == View.VISIBLE){
             val builder = AlertDialog.Builder(this)
-            builder.setTitle("¿Estas seguro que desea salir?")
+            builder.setTitle("¿Esta seguro que desea salir?")
             builder.setMessage("Si abandonas el registro, los datos que había ingresado se perderán.")
-            builder.setPositiveButton("Salir"){_, _->
+            builder.setPositiveButton("Salir"){_, _ ->
                 finish()
             }
-
-            builder.setNegativeButton("Continuar"){dialog, _ ->
+            builder.setNegativeButton("Continuar"){ dialog, _ ->
                 dialog.dismiss()
             }
-
             val dialog = builder.create()
             dialog.show()
         }
